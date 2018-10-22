1d98db2bbdb68cfc05316cc255f30b278167e36e
Roman Mulyk <work.mulykr@gmail.com>
2018-10-22 18:39:58 +0300

    Working version

---
+++ Forms/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Threading;
+using System.Threading;
+using System.Windows.Media.Animation;
+
+namespace Forms
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+            : base()
+        {
+            InitializeComponent();
+        }
+
+        private void NewWindow(object sender, RoutedEventArgs e)
+        {
+            Thread newThread = new Thread(new ThreadStart(StartPoint));
+            newThread.SetApartmentState(ApartmentState.STA);
+            newThread.IsBackground = true;
+            newThread.Start();
+        }
+
+        private void StartPoint()
+        {
+            MainWindow temp = new MainWindow();
+            temp.Show();
+            Dispatcher.Run();
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            Duration duration = new Duration(TimeSpan.FromSeconds(10));
+            DoubleAnimation doubleanimation = new DoubleAnimation(100.0, duration);
+            progressBar.BeginAnimation(ProgressBar.ValueProperty, doubleanimation);
+        }
+    }
+}
